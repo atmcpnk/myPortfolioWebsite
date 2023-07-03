bb5aa04be25a2959e523e7f22bcb1c8490e76c15
abrahamflores <a.flrs182@gmail.com>
2023-07-03 14:44:00 -0700

    added new content to the projects tab, imported the footer

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Routes, Route, NavLink } from 'react-router-dom';
+import Footer from './Footer';
 
 const Projects = () => {
     return (
         <>
+            <div>
+                <h1>projects 👨‍💻</h1>
+            </div>
+            <footer>
+                <Footer />
+            </footer>
         </>
     )
 };

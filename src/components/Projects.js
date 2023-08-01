846ab9e0e0f320acaf3d6d185059e46a8e0ed572
abrahamflores <a.flrs182@gmail.com>
2023-08-01 13:13:54 -0700

    added a description to the second project in the projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,14 +9,14 @@ const Projects = () => {
                 <h1>projects 👨‍💻</h1>
                 <div>
                     <h2>my portfolio site!</h2>
-                    <p>This site was built by me utilizing a react framework.  </p>
+                    <p>this site was built by me utilizing a react framework.  </p>
                 </div>
                 <div>
-                    <h2>project 2</h2>
-                    <p>project 2 description</p>
+                    <h2>infinite worlds</h2>
+                    <p>infinite worlds is a side scrolling platformer game, similar to those we grew up with from the mid 1990's. built using phaser.js, this project was exceptionally challenging. As a team we worked together to make this idea come to life. splitting the project into four parts I was in charge of creating and designing the scenes before and after the main game, these are the title scene, loading screen, controller scene, game over, and credit scenes.</p>
                 </div>
                 <div>
-                    <h2>project 3</h2>
+                    <h2>gracebuster e-commerce</h2>
                     <p>project 3 description</p>
                 </div>
             </div>

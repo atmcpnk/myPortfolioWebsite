4e99ea0b832bf6023e6323c32f1e1d326a0b7368
abrahamflores <a.flrs182@gmail.com>
2023-08-04 11:26:42 -0700

    updated the home page and main.js file to redesign how the site is going to be set up

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,9 @@ const Home = () => {
         <>
             <div>
                 <main>
-                    <h1 id="home-title">hello 👋</h1>
-                    {/* selfie / headshot here offset to the side, maybe place this in a div */}
-                    <h3>welcome to my site</h3>
-                    <p>short bio on myself</p>
+                    <h1 id="home-title">abraham flores</h1>
+                    <h3>fullstack developer</h3>
+                    {/* <p>short bio on myself</p> */}
                 </main>
             </div>
             <footer>

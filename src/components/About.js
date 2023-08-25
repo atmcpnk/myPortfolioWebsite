ab26f419e4a8d9837f785b64f9108d4fbc9fa0cd
abrahamflores <a.flrs182@gmail.com>
2023-08-25 15:00:49 -0700

    getting started on the about page styling

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,12 @@ const About = () => {
         <>
             <div>
                 <div>
-                    <h1>about me</h1>
+                    <h1>about me 📝</h1>
                     <p>short bio on my experiences</p>
                 </div>
                 {/* picture here either below or to the right of the paragraph */}
                 <div>
-                    <h1>other than coding</h1>
+                    <h1>other than coding 🎸</h1>
                     <p>short bio on my hobbies</p>
                 </div>
             </div>

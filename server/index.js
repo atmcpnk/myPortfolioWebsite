4d614e15c3e6568cc6a9e4a5017240d9f9427712
apophis51 <malcolmxvernon@hotmail.com>
2023-08-26 06:05:53 -0400

    jhkj

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path')
+
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'public/index.html')));
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

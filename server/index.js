155caffe9b7ad31a0dc45208b5c8fd65136c6a3e
apophis51 <malcolmxvernon@hotmail.com>
2023-08-26 07:53:39 -0400

    fgsg

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const app = require('./app')
 
-const port = 8080;
+const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'public/index.html')));
 

52c21f252c2a53de18f490b29bf7a9fe432f6c74
apophis51 <malcolmxvernon@hotmail.com>
2023-08-26 08:01:03 -0400

    jgjhgj

--- server/index.js
+++ server/index.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 8080
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'public/index.html')));
 
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });

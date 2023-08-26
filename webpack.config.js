a74d29d9bc827c85ba3ef1f507fdd2161cf3fb6f
apophis51 <malcolmxvernon@hotmail.com>
2023-08-26 05:53:31 -0400

    dd

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-    entry: ['./server/index.js'],
-    output: {
-      path: __dirname + '/public',
-      filename: 'bundle.js',
-    },
-    context: __dirname,
-    devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
+  entry: ['./client/index.js'],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js',
+  },
+  context: __dirname,
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
-      ],
-    },
-  };
+      },
+    ],
+  },
+};
